ed55da0616742e78fba0e0846cc72e97ff6b9f77
AIASLab <aiaslaboratory@gmail.com>
2024-11-03 02:35:20 +0800

    Update evaluate_fdfr_ism.sh

--- scripts/evaluate_fdfr_ism.sh
+++ scripts/evaluate_fdfr_ism.sh
@@ -3,7 +3,7 @@
 
 exec > >(cat) 2> results/evaluate_fdfr_ism_error.txt
 
-export DATA_PATH="./infer/gap_diff_per16_jpeg70" 
+export DATA_PATH="./infer/gap_diff_per16" 
 
 export TF_ENABLE_ONEDNN_OPTS=0
 time python evaluations/full_ism_fdfr.py --data_path=$DATA_PATH --method="GAP-Diff"
